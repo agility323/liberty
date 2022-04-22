577d0304877b72727c4877fd54e60a8570bc7ad6
Thomas Wade <wutian323@126.com>
2022-04-22 15:45:33 +0800

    feat: simplify etcd test code

--- lbtreg/etcd_test.go
+++ lbtreg/etcd_test.go
@@ -21,28 +21,15 @@ func TestPrintEtcd(t *testing.T){
 	}
 	fmt.Println("connect to etcd success")
 	defer cli.Close()
-	// put
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	_, err = cli.Put(ctx, "q1mi1", "dsb")
-	cancel()
-	if err != nil {
-		fmt.Printf("put to etcd failed, err:%v\n", err)
-		return
-	}
-	// get
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	//resp, err := cli.Get(ctx, "Zm9v")
-	resp, err := cli.Get(ctx, "", clientv3.WithPrefix())
-	fmt.Printf("resp %v\n", resp)
-	cancel()
+	resp, err := cli.Get(context.Background(), "", clientv3.WithPrefix())
+	//fmt.Printf("resp %v\n", resp)
 	if err != nil {
 		fmt.Printf("get from etcd failed, err:%v\n", err)
 		return
 	}
 	for _, ev := range resp.Kvs {
 		fmt.Printf("%v:%v\n", string(ev.Key), string(ev.Value))
-		fmt.Printf("%v:%v\n", (ev.Key), (ev.Value))
+		//fmt.Printf("%v:%v\n", (ev.Key), (ev.Value))
 	}
-	//cli.
 }
 
