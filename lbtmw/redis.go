3d7e46ae855090966782b90a98263264cefda6db
Thomas Wade <wutian323@126.com>
2023-01-03 20:54:29 +0800

    feat: expose RedisClient to service

--- lbtmw/redis.go
+++ lbtmw/redis.go
@@ -8,17 +8,17 @@ import (
 
 const RedisKeyDelimiter = ":"
 
-var redisClient redis.UniversalClient
+var RedisClient redis.UniversalClient
 
 // 1. If the MasterName option is specified, a sentinel-backed FailoverClient is returned.
 // 2. if the number of Addrs is two or more, a ClusterClient is returned.
 // 3. Otherwise, a single-node Client is returned.
 func InitRedisClient(addrs []string, masterName string) {
-	redisClient = redis.NewUniversalClient(&redis.UniversalOptions{
+	RedisClient = redis.NewUniversalClient(&redis.UniversalOptions{
 		Addrs: addrs,
 		MasterName: masterName,
 	})
-	if err := redisClient.Ping().Err(); err != nil {
+	if err := RedisClient.Ping().Err(); err != nil {
 		panic(err)
 	}
 	logger.Info("init redis %v %s", addrs, masterName)
