3f318edd516a0e061e408027981812b86cdfb501
Thomas Wade <wutian323@126.com>
2023-01-04 16:50:04 +0800

    feat: hotfix add MethodFuncEntry type.

--- hotfix/entry.go
+++ hotfix/entry.go
@@ -1,12 +1,18 @@
 package hotfix
 
 import (
+	"github.com/agility323/liberty/hotfix/itf"
+
 	"github.com/agiledragon/gomonkey/v2"
 )
 
 type FuncEntry struct {
 	target interface{}
 	double interface{}
+}
+
+func (h *HotfixImpl) NewFuncEntry(target interface{}, double interface{}) itf.HotfixEntry {
+	return &FuncEntry{target: target, double: double}
 }
 
 func (e *FuncEntry) Apply() {
@@ -19,6 +25,24 @@ type MethodEntry struct {
 	double interface{}
 }
 
+func (h *HotfixImpl) NewMethodEntry(target interface{}, method string, double interface{}) itf.HotfixEntry {
+	return &MethodEntry{target: target, method: method, double: double}
+}
+
 func (e *MethodEntry) Apply() {
 	gomonkey.ApplyMethod(e.target, e.method, e.double)
+}
+
+type MethodFuncEntry struct {
+	target interface{}
+	method string
+	double interface{}
+}
+
+func (h *HotfixImpl) NewMethodFuncEntry(target interface{}, method string, double interface{}) itf.HotfixEntry {
+	return &MethodFuncEntry{target: target, method: method, double: double}
+}
+
+func (e *MethodFuncEntry) Apply() {
+	gomonkey.ApplyMethodFunc(e.target, e.method, e.double)
 }
--- hotfix/hotfix.go
+++ hotfix/hotfix.go
@@ -14,14 +14,6 @@ type HotfixImpl struct {}
 
 var Hotfix *HotfixImpl
 
-func (h *HotfixImpl) NewFuncEntry(target interface{}, double interface{}) itf.HotfixEntry {
-	return &FuncEntry{target: target, double: double}
-}
-
-func (h *HotfixImpl) NewMethodEntry(target interface{}, method string, double interface{}) itf.HotfixEntry {
-	return &MethodEntry{target: target, method: method, double: double}
-}
-
 func (h *HotfixImpl) ApplyHotfix(entries []itf.HotfixEntry) {
 	logger.Info("apply hotfix begin %d", len(entries))
 	for _, entry := range entries {
