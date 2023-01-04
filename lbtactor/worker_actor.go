78fb2601dd8f886a9bb29fa4e0f91645da288829
Thomas Wade <wutian323@126.com>
2023-01-04 14:52:25 +0800

    fix: worker_actor send to close panic

--- lbtactor/worker_actor.go
+++ lbtactor/worker_actor.go
@@ -1,14 +1,9 @@
 package lbtactor
 
 import (
-	"github.com/agility323/liberty/lbtutil"
-	"os"
-	"strconv"
 	"sync/atomic"
 	"time"
 )
-
-var logger = lbtutil.NewLogger(strconv.Itoa(os.Getpid()), "lbtactor")
 
 type taskWithNoReturn func()
 
@@ -16,30 +11,27 @@ type WorkerActor struct {
 	state int32
 	activet int32
 	taskq chan taskWithNoReturn
-	done chan struct{}
-	closed bool
+	stopq chan struct{}
 }
 
 func NewWorkerActor() *WorkerActor {
 	return &WorkerActor{
 		state: 0,
 		taskq: nil,
-		done: nil,
-		closed: false,
+		stopq: nil,
 	}
 }
 
 func (w *WorkerActor) Start(qlen int) bool {
 	if !atomic.CompareAndSwapInt32(&w.state, 0, 1) { return false }
 	w.taskq = make(chan taskWithNoReturn, qlen)
-	w.done = make(chan struct{} , 1)
+	w.stopq = make(chan struct{}, 1)
 	go func() {
 		for {
 			select {
-			case <-w.done:
-				w.closed = true
+			case <-w.stopq:
 				return
-			case task:=<-w.taskq:
+			case task := <-w.taskq:
 				task()
 			}
 		}
@@ -49,21 +41,23 @@ func (w *WorkerActor) Start(qlen int) bool {
 
 func (w *WorkerActor) Stop() bool {
 	if !atomic.CompareAndSwapInt32(&w.state, 1, 2) { return false }
-	w.done <- struct{}{}
-	return true
+	select {
+	case w.stopq<- struct{}{}:
+		return true
+	default:
+		return false
+	}
 }
 
 func (w *WorkerActor) PushTask(task taskWithNoReturn) bool {
-	if w.closed{
-		logger.Error("work_actor taskq is closed")
+	if w.state == 2 {
 		return false
 	}
 	select {
-	case w.taskq <- task:
+	case w.taskq<- task:
 		atomic.StoreInt32(&w.activet, int32(time.Now().Unix()))
 		return true
 	default:
-		logger.Error("work_actor taskq is full")
 		return false
 	}
 	return false
