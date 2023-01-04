f8c20a5845e70a78387bfdb293775ff48228757b
Thomas Wade <wutian323@126.com>
2023-01-04 14:28:36 +0800

    fix: update hotfix test.

--- hotfix/hotfix_test.go
+++ hotfix/hotfix_test.go
@@ -30,12 +30,12 @@ func NewSF(s *S) string {
 
 func TestHotfix(t *testing.T) {
 	entries := []interface{} {
-		NewFuncEntry(F, NewF),
-		NewMethodEntry((*S)(nil), "F", NewSF),
+		Hotfix.NewFuncEntry(F, NewF),
+		Hotfix.NewMethodEntry((*S)(nil), "F", NewSF),
 	}
 	fmt.Printf("old F: %v\n", F())
 	fmt.Printf("old s.F: %v\n", (*S)(nil).F())
-	ApplyHotfix(entries)
+	Hotfix.ApplyHotfix(entries)
 	fmt.Printf("new F: %v\n", F())
 	fmt.Printf("new s.F: %v\n", (*S)(nil).F())
 }
