03daf6f53dedb56df2be45eeb205aae0c06ad215
Thomas Wade <wutian323@126.com>
2023-01-04 14:44:02 +0800

    feat: 	add logger_sdk 	update log

--- lbtutil/log.go
+++ lbtutil/log.go
@@ -39,74 +39,61 @@ type logger struct {
 
 var log = NewLogger(strconv.Itoa(os.Getpid()), "lbtutil")
 
-func NewLogger(prefix string, tag string) Logger { return &logger{prefix: prefix, tag: tag} }
+func NewLogger(prefix string, tag string) Logger { return &logger{prefix: fmt.Sprintf("%s %s", prefix, tag), tag: tag} }
 
 func (l *logger) Debug(format string, params ...interface{}) {
-	logByLevel(Ldebug, "DEBUG", l.prefix, l.tag, format, params...)
+	logByLevel(Ldebug, "DEBUG", l.prefix, format, params...)
 }
 
 func (l *logger) Info(format string, params ...interface{}) {
-	logByLevel(Linfo, "INFO", l.prefix, l.tag, format, params...)
+	logByLevel(Linfo, "INFO", l.prefix, format, params...)
 }
 
 func (l *logger) Warn(format string, params ...interface{}) {
-	logByLevel(Lwarn, "WARN", l.prefix, l.tag, format, params...)
+	logByLevel(Lwarn, "WARN", l.prefix, format, params...)
 }
 
 func (l *logger) Error(format string, params ...interface{}) {
-	logByLevel(Lerror, "ERROR", l.prefix, l.tag, format, params...)
+	logByLevel(Lerror, "ERROR", l.prefix, format, params...)
 }
 
 
-func logByLevel(level int, lvstr, prefix, tag, format string, params ...interface{}) {
+func logByLevel(level int, lvstr, prefix, format string, params ...interface{}) {
 	// check level
 	if level < int(atomic.LoadInt32(&logLevel)) { return }
 	// time str
 	now := time.Now()
 	tstr := fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d",
-			now.Year(), now.Month(), now.Day(),
-			now.Hour(), now.Minute(), now.Second())
+		now.Year(), now.Month(), now.Day(),
+		now.Hour(), now.Minute(), now.Second())
 	// format
 	body := fmt.Sprintf(format, params...)
 	// build str
-	n := len(tstr) + 1 + len(prefix) + 1 + len(lvstr) + 1 + len(tag) + 1 + len(body) + 1
+	var sepStr = "] ["
+	pathInfo := ""
+	n := 1 + len(tstr) + 3 + len(lvstr) + 3 + len(prefix) + 3 + len(body) + 3 + len(pathInfo) + 2
 	b := make([]byte, n, n)
-	pos := 0
-	var sep byte = ' '
+
+	b[0] = '['
+	pos := 1
 	copy(b[pos:], tstr)
 	pos += len(tstr)
-	b[pos] = sep
-	pos += 1
-	copy(b[pos:], prefix)
-	pos += len(prefix)
-	b[pos] = sep
-	pos += 1
+	copy(b[pos:], sepStr)
+	pos += len(sepStr)
 	copy(b[pos:], lvstr)
 	pos += len(lvstr)
-	b[pos] = sep
-	pos += 1
-	copy(b[pos:], tag)
-	pos += len(tag)
-	b[pos] = sep
-	pos += 1
+	copy(b[pos:], sepStr)
+	pos += len(sepStr)
+	copy(b[pos:], prefix)
+	pos += len(prefix)
+	copy(b[pos:], sepStr)
+	pos += len(sepStr)
 	copy(b[pos:], body)
 	pos += len(body)
-	b[pos] = '\n'
-	/*
-	sb := strings.Builder{}
-	sb.Grow(len(prefix) + 1 + len(tstr) + 1 + len(tag) + 1 + len(body) + 1)
-	sep := " "
-	sb.WriteString(tstr)
-	sb.WriteString(sep)
-	sb.WriteString(prefix)
-	sb.WriteString(sep)
-	sb.WriteString(lvstr)
-	sb.WriteString(sep)
-	sb.WriteString(tag)
-	sb.WriteString(sep)
-	sb.WriteString(body)
-	sb.WriteString("\n")
-	*/
-	// write
+	copy(b[pos:], sepStr)
+	pos += len(sepStr)
+	copy(b[pos:], pathInfo)
+	pos += len(pathInfo)
+	copy(b[pos:], "]\n")
 	logOut.Write(b)
 }
--- lbtutil/log_test.go
+++ lbtutil/log_test.go
@@ -7,4 +7,6 @@ import (
 func TestLogWithTag(t *testing.T) {
 	BICoreLog("login", map[string]interface{}{"event":"login", "detail":[]int{1,2,3,4}})
 	BICustomLog("login", map[string]interface{}{"event":"login", "detail":[]int{1,2,3,4}})
+	logger1 := NewLogger("avatar", "building")
+	logger1.Info("test sdfsdfwer")
 }
