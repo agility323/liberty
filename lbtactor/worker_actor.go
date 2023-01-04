4f40af7150d44f07c336d96963cc779ec114667e
Thomas Wade <wutian323@126.com>
2023-01-04 14:50:23 +0800

    fix: worker_actor send to close panic

--- lbtactor/worker_actor.go
+++ lbtactor/worker_actor.go
@@ -11,21 +11,32 @@ type WorkerActor struct {
 	state int32
 	activet int32
 	taskq chan taskWithNoReturn
+	done chan struct{}
+	closed bool
 }
 
 func NewWorkerActor() *WorkerActor {
 	return &WorkerActor{
 		state: 0,
 		taskq: nil,
+		done: nil,
+		closed: false,
 	}
 }
 
 func (w *WorkerActor) Start(qlen int) bool {
 	if !atomic.CompareAndSwapInt32(&w.state, 0, 1) { return false }
 	w.taskq = make(chan taskWithNoReturn, qlen)
+	w.done = make(chan struct{} , 1)
 	go func() {
-		for task := range w.taskq {
-			task()
+		for {
+			select {
+			case <-w.done:
+				w.closed = true
+				return
+			case task:=<-w.taskq:
+				task()
+			}
 		}
 	} ()
 	return true
@@ -33,11 +44,14 @@ func (w *WorkerActor) Start(qlen int) bool {
 
 func (w *WorkerActor) Stop() bool {
 	if !atomic.CompareAndSwapInt32(&w.state, 1, 2) { return false }
-	close(w.taskq)
+	w.done <- struct{}{}
 	return true
 }
 
 func (w *WorkerActor) PushTask(task taskWithNoReturn) bool {
+	if w.closed{
+		return false
+	}
 	select {
 	case w.taskq <- task:
 		atomic.StoreInt32(&w.activet, int32(time.Now().Unix()))
